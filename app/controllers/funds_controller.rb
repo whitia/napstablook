ebffbcb153ea29a341faa5a7d2721d8d1248de4d
whitia <whitia7@gmail.com>
2019-09-28 14:26:25 +0900

    Modified import

--- app/controllers/funds_controller.rb
+++ app/controllers/funds_controller.rb
@@ -17,25 +17,49 @@ class FundsController < ApplicationController
   end
 
   def import
-    Fund.where(user_id: params[:user_id]).destroy_all
+    # Import
     funds = []
     CSV.foreach(params[:file].path, headers: true, encoding: 'Shift_JIS:UTF-8') do |row|
       funds << Fund.new(name: row['ファンド名'],
-                              category: nil,
-                              account: row['口座種別'],
-                              purchase: row['買付金額'],
-                              valuation: row['評価金額'],
-                              user_id: params[:user_id])
+                            category: nil,
+                            account: row['口座種別'],
+                            purchase: row['買付金額'],
+                            valuation: row['評価金額'],
+                            user_id: params[:user_id])
     end
+    funds.each do |row|
+      if Fund.where(user_id: params[:user_id])
+             .where(name: row.name)
+             .where(account: row.account)
+             .where.not(category: nil).count > 0
+        row.category = Fund.where(user_id: params[:user_id])
+                           .where(name: row.name)
+                           .where(account: row.account)
+                           .where.not(category: nil)[0].category
+      end
+    end
+    Fund.where(user_id: params[:user_id]).destroy_all
     Fund.import(funds)
+
+    # Return
     @funds = Fund.where(user_id: params[:user_id]).order('account DESC')
     redirect_to user_funds_path
   end
 
   def set_category
     fund = Fund.find(params[:id])
-    fund.update(category: params[:category])
+    fund.update(category: (params[:category] == '' ? nil : params[:category]))
     render json: { status: 'success' }
   end
+
+  private
+    def is_exists(funds_org, funds_new)
+      org.foreach do |row|
+        if row.name == funds_new['ファンド名'] && row.account == funds_new['口座種別']
+          return true
+        end
+      end
+      return false
+    end
 
 end
