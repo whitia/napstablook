952d3ec4384e65a372750b5cf63f7c0dd2dfe25d
whitia <whitia7@gmail.com>
2019-11-20 14:39:48 +0900

    Modified spec of user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,4 +30,30 @@ RSpec.describe User, type: :model do
     user.valid?
     expect(user.errors[:email]).to include('has already been taken')
   end
+
+  it 'is invalid without a password' do
+    user = User.new(password: nil, password_confirmation: nil)
+    user.valid?
+    expect(user.errors[:password]).to include('can\'t be blank')
+  end
+
+  it 'is valid with a password length is 6 characters' do
+    user = User.new(
+      email: 'test@example.com',
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user.valid?
+    expect(user).to be_valid
+  end
+
+  it 'is invalid with a password length is 5 characters' do
+    user = User.new(
+      email: 'test@example.com',
+      password: '12345',
+      password_confirmation: '12345'
+    )
+    user.valid?
+    expect(user.errors[:password]).to include('is too short (minimum is 6 characters)')
+  end
 end
