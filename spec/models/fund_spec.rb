1193167986b7a07d49bf18b8e84dd7efe3b15ae4
whitia <whitia7@gmail.com>
2019-11-20 22:18:18 +0900

    Modified spec of fund model

--- spec/models/fund_spec.rb
+++ spec/models/fund_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Fund, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:fund)).to be_valid
   end
+
+  it 'is associated user when created fund' do
+    expect(FactoryBot.build(:fund).user_id).to eq 1
+  end
 end
