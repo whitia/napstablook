ca3984253afbf5abeb7b0d1ad3271b8a521535e4
whitia <whitia7@gmail.com>
2019-11-22 18:32:10 +0900

    Modified spec of users feature

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -15,4 +15,21 @@ RSpec.feature "Users", type: :feature do
       expect(page).to have_content user.email
     }.to change(User, :count).by(1)
   end
+
+  scenario 'user deletes a user' do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link 'ログイン'
+    fill_in 'メールアドレス', with: user.email
+    fill_in 'パスワード', with: user.password
+    click_button 'ログイン'
+
+    expect{
+      click_button '削除'
+      page.driver.browser.switch_to.alert.accept
+
+      expect(page).to have_content '投資信託管理サービス'
+    }.to change(User, :count).by(-1)
+  end
 end
