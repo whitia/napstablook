e9b0c4bf4ad825a1288fea02e7b8206921d9ccc3
whitia <whitia7@gmail.com>
2019-12-08 17:29:08 +0900

    Modified application name

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FundList
+module Napstablook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
